b2e0504526dc8338d7d8afdea3483f911c7d2c19
lmv5906-maker <lmv5906@gmail.com>
2026-03-18 16:23:56 +0300

    Update header.js

--- js/components/header.js
+++ js/components/header.js
@@ -1,9 +1,15 @@
-import { isAuthed } from '../storage/auth.js';
+import { isAuthed, isSeller } from '../storage/auth.js';
 
 export function initHeader() {
   const profileLink = document.querySelector('[data-role="profile-link"]');
   if (profileLink) {
-    profileLink.setAttribute('href', isAuthed() ? 'user.html' : 'login.html');
+    if (!isAuthed()) {
+      profileLink.setAttribute('href', 'login.html');
+    } else if (isSeller()) {
+      profileLink.setAttribute('href', 'seller-dashboard.html');
+    } else {
+      profileLink.setAttribute('href', 'user.html');
+    }
   }
 
   document.querySelectorAll('[data-go]').forEach((el) => {
